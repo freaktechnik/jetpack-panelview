e8b7b7bb4d4561b251afa2cc8af2f1b017f1f449
Martin Giger <martin@humanoids.be>
2016-03-26 11:02:47 +0100

    Switch demo to toggle button and add workaround, since it's needed for buttons inside panels (like the hamburger menu)

--- demo/index.js
+++ demo/index.js
@@ -1,52 +1,64 @@
-var { PanelView } = require("jetpack-panelview");
-var buttons = require('sdk/ui/button/action');
+const { PanelView } = require("jetpack-panelview");
+const { ToggleButton } = require('sdk/ui');
+const workaround = require("jetpack-panelview/lib/panelview/workaround");
 
-var pv = PanelView({
-        id: 'demo-panelview',
-        title: 'Demo',
-        content: [
-            {
-                type: 'button',
-                label: 'First Action',
-                onClick: function(event) {
-                    // action handler for the first action
-                }
-            },
-            {
-                type: 'button',
-                label: 'Second Action',
-                onClick: function(event) {
-                    // handler for the second event
-                }
-            },
-            {
-                type: 'separator'
-            },
-            {
-                type: 'button',
-                label: 'Separated Button',
-                onClick: function(event) {
-                    // another event handler
-                }
+const pv = PanelView({
+    id: 'demo-panelview',
+    title: 'Demo',
+    content: [
+        {
+            type: 'button',
+            label: 'First Action',
+            onClick(event) {
+                // action handler for the first action
             }
-        ],
-        footer: {
-            label: 'Footer Action',
-            onClick: function(event) {
-                // footer click handler
+        },
+        {
+            type: 'button',
+            label: 'Second Action',
+            onClick(event) {
+                // handler for the second event
+            }
+        },
+        {
+            type: 'separator'
+        },
+        {
+            type: 'button',
+            label: 'Separated Button',
+            onClick(event) {
+                // another event handler
             }
         }
-    });
+    ],
+    footer: {
+        label: 'Footer Action',
+        onClick(event) {
+            // footer click handler
+        }
+    }
+});
 
-    var button = buttons.ActionButton({
-      id: "demo-button",
-      label: "Jetpack PanelView",
-      icon: {
-        "16": "./icon-16.png",
-        "32": "./icon-32.png",
-        "64": "./icon-64.png"
-      },
-        onClick: function(state){
+const button = ToggleButton({
+  id: "demo-button",
+  label: "Jetpack PanelView",
+  icon: {
+    "16": "./icon-16.png",
+    "32": "./icon-32.png",
+    "64": "./icon-64.png"
+  },
+    onClick(state){
+        if(state.checked) {
             pv.show(button);
         }
-    });
+    }
+});
+
+// Uncheck the button if the panel is hidden by loosing focus
+pv.on("hide", () => {
+    button.state("window", {checked: false});
+});
+
+// Don't close the menu panel or overflow panel when the button is clicked.
+workaround.applyButtonFix(button);
+
